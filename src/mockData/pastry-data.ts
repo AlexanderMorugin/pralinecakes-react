acd6fc43102d8ecba14424b63674be8793cce6b2
Alexander Morugin <nobilis@bk.ru>
2024-06-12 18:54:19 +0300

    делаю базу пирожных

--- src/mockData/pastry-data.ts
+++ src/mockData/pastry-data.ts
@@ -13,8 +13,54 @@ import RaspberryBasketL from '../assets/images/pastry/pastry-raspberry-basket-l.
 import WildBerriesBasketS from '../assets/images/pastry/pastry-wild-berry-basket-s.jpeg';
 import WildBerriesBasketM from '../assets/images/pastry/pastry-wild-berry-basket-m.jpeg';
 import WildBerriesBasketL from '../assets/images/pastry/pastry-wild-berry-basket-l.jpeg';
+import StrudelYablochniyS from '../assets/images/pastry/pastry-apple-strudel-s.jpeg';
+import StrudelYablochniyM from '../assets/images/pastry/pastry-apple-strudel-m.jpeg';
+import StrudelYablochniyL from '../assets/images/pastry/pastry-apple-strudel-l.jpeg';
+import StrudelMakoviyS from '../assets/images/pastry/pastry-makoviy-strudel-s.jpeg';
+import StrudelMakoviyM from '../assets/images/pastry/pastry-makoviy-strudel-m.jpeg';
+import StrudelMakoviyL from '../assets/images/pastry/pastry-makoviy-strudel-l.jpeg';
+import StrudelVishneviyS from '../assets/images/pastry/pastry-cherry-strudel-s.jpeg';
+import StrudelVishneviyM from '../assets/images/pastry/pastry-cherry-strudel-m.jpeg';
+import StrudelVishneviyL from '../assets/images/pastry/pastry-cherry-strudel-l.jpeg';
+import EklerS from '../assets/images/pastry/pastry-eclery-s.jpeg';
+import EklerM from '../assets/images/pastry/pastry-eclery-m.jpeg';
+import EklerL from '../assets/images/pastry/pastry-eclery-l.jpeg';
+import CakePopS from '../assets/images/pastry/pastry-cake-pops-s.jpeg';
+import CakePopM from '../assets/images/pastry/pastry-cake-pops-m.jpeg';
+import CakePopL from '../assets/images/pastry/pastry-cake-pops-l.jpeg';
+import CheesecakeBlueberryS from '../assets/images/pastry/pastry-cheesecake-blue-s.jpeg';
+import CheesecakeBlueberryM from '../assets/images/pastry/pastry-cheesecake-blue-m.jpeg';
+import CheesecakeBlueberryL from '../assets/images/pastry/pastry-cheesecake-blue-l.jpeg';
+import FantasiaS from '../assets/images/pastry/pastry-fantasia-s.jpeg';
+import FantasiaM from '../assets/images/pastry/pastry-fantasia-m.jpeg';
+import FantasiaL from '../assets/images/pastry/pastry-fantasia-l.jpeg';
+import PtichieMolokoS from '../assets/images/pastry/pastry-ptichie-moloko-s.jpeg';
+import PtichieMolokoM from '../assets/images/pastry/pastry-ptichie-moloko-m.jpeg';
+import PtichieMolokoL from '../assets/images/pastry/pastry-ptichie-moloko-l.jpeg';
+import MilfeyDjanduaS from '../assets/images/pastry/pastry-djanduya-s.jpeg';
+import MilfeyDjanduaM from '../assets/images/pastry/pastry-djanduya-m.jpeg';
+import MilfeyDjanduaL from '../assets/images/pastry/pastry-djanduya-l.jpeg';
+import MindalnoeS from '../assets/images/pastry/pastry-mindalnoe-s.jpeg';
+import MindalnoeM from '../assets/images/pastry/pastry-mindalnoe-m.jpeg';
+import MindalnoeL from '../assets/images/pastry/pastry-mindalnoe-l.jpeg';
+import KartoshkaS from '../assets/images/pastry/pastry-kartoshka-s.jpeg';
+import KartoshkaM from '../assets/images/pastry/pastry-kartoshka-m.jpeg';
+import KartoshkaL from '../assets/images/pastry/pastry-kartoshka-l.jpeg';
+import KaramelnoeS from '../assets/images/pastry/pastry-caramel-s.jpeg';
+import KaramelnoeM from '../assets/images/pastry/pastry-caramel-m.jpeg';
+import KaramelnoeL from '../assets/images/pastry/pastry-caramel-l.jpeg';
+import BavarskoeS from '../assets/images/pastry/pastry-bavarskoe-s.jpeg';
+import BavarskoeM from '../assets/images/pastry/pastry-bavarskoe-m.jpeg';
+import BavarskoeL from '../assets/images/pastry/pastry-bavarskoe-l.jpeg';
+import MaraquyaS from '../assets/images/pastry/pastry-maraquya-s.jpeg';
+import MaraquyaM from '../assets/images/pastry/pastry-maraquya-m.jpeg';
+import MaraquyaL from '../assets/images/pastry/pastry-maraquya-l.jpeg';
+import TriShokoladaS from '../assets/images/pastry/pastry-tri-shokolada-s.jpeg';
+import TriShokoladaM from '../assets/images/pastry/pastry-tri-shokolada-m.jpeg';
+import TriShokoladaL from '../assets/images/pastry/pastry-tri-shokolada-l.jpeg';
 
 
+TriShokoladaS
 export const pastryData = [
   {
     id: 0,
@@ -129,6 +175,351 @@ export const pastryData = [
     weight_b: 45,
     price: 49,
     price_b: 70,
+    category: 'pastry',
+  },
+  {
+    id: 5,
+    route: 'strudel-yablochniy',
+    image_small: StrudelYablochniyS,
+    image_normal: StrudelYablochniyM,
+    image_large: StrudelYablochniyL,
+    title: 'Штрудель яблочный',
+    description:
+      'Штрудель из тонкого теста с начинкой из яблок, изюма и грецких орехов',
+    ingredients:
+      'Яблоко, мука, масло подсолнечное, соль, сахар, изюм, сахар ванильный',
+    protein: 15.9,
+    fat: 29.7,
+    carbohydrates: 26.5,
+    calories: 290,
+    quantity: 15,
+    quantity_b: 15,
+    weight: 100,
+    weight_b: 140,
+    price: 77,
+    price_b: 100,
+    category: 'pastry',
+  },
+  {
+    id: 6,
+    route: 'strudel-makoviy',
+    image_small: StrudelMakoviyS,
+    image_normal: StrudelMakoviyM,
+    image_large: StrudelMakoviyL,
+    title: 'Штрудель маковый',
+    description:
+      'Штрудель из тонкого теста с начинкой из мака и грецкого ореха',
+    ingredients:
+      'Мак пищевой, миндальная мука, мука пшеничная, мед, сахар песок, грецкий орех, масло растительное, сахарная пудра нетающая, соль',
+    protein: 4.3,
+    fat: 25.3,
+    carbohydrates: 33.4,
+    calories: 379,
+    quantity: 15,
+    quantity_b: 15,
+    weight: 100,
+    weight_b: 140,
+    price: 82,
+    price_b: 103,
+    category: 'pastry',
+  },
+  {
+    id: 7,
+    route: 'strudel-vishneviy',
+    image_small: StrudelVishneviyS,
+    image_normal: StrudelVishneviyM,
+    image_large: StrudelVishneviyL,
+    title: 'Штрудель вишневый',
+    description:
+      'Аппетитное сочетание вишни и грецких орехов в рулете из тончайшего теста',
+    ingredients:
+      'Вишня с/м, мука, сахар, орех грецкий, подсолнечное масло, соль',
+    protein: 4.2,
+    fat: 19.5,
+    carbohydrates: 27.9,
+    calories: 290,
+    quantity: 15,
+    quantity_b: 15,
+    weight: 100,
+    weight_b: 140,
+    price: 82,
+    price_b: 104,
+    category: 'pastry',
+  },
+  {
+    id: 8,
+    route: 'ekler',
+    image_small: EklerS,
+    image_normal: EklerM,
+    image_large: EklerL,
+    title: 'Эклер',
+    description:
+      'Шоколадный, фисташковый или ванильный',
+    ingredients:
+      'Молоко, мука пшеничная, масло сливочное 82%, молоко сгущенное гост, сахар ванильный, масло растительное, крем заварной',
+    protein: 5.2,
+    fat: 8.9,
+    carbohydrates: 52.5,
+    calories: 380,
+    quantity: 10,
+    quantity_b: null,
+    weight: 50,
+    weight_b: null,
+    price: 70,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 9,
+    route: 'cake-pops',
+    image_small: CakePopS,
+    image_normal: CakePopM,
+    image_large: CakePopL,
+    title: 'Кейк попс',
+    description:
+      'Небольшое шоколадное бисквитное пирожное в глазури на палочке',
+    ingredients:
+      'Шоколад 53%, сливки 33%, сахар, мука пшеничная, яйцо куриное, джем абрикосовый, какао, масло растительное',
+    protein: 6.4,
+    fat: 21.2,
+    carbohydrates: 46.7,
+    calories: 369,
+    quantity: 30,
+    quantity_b: null,
+    weight: 35,
+    weight_b: null,
+    price: 86,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 10,
+    route: 'cheesecake-blueberry',
+    image_small: CheesecakeBlueberryS,
+    image_normal: CheesecakeBlueberryM,
+    image_large: CheesecakeBlueberryL,
+    title: 'Чизкейк с голубикой',
+    description:
+      'Легкое сырное суфле со свежей голубикой на тонком бисквите',
+    ingredients:
+      'Сыр творожный, яйцо куриное, мука пшеничная, сахар, сметана, йогурт, сахарная пудра, желатин, сливки 33%, голубика, гель нейтральный',
+    protein: 6.5,
+    fat: 11.6,
+    carbohydrates: 29.5,
+    calories: 175,
+    quantity: 16,
+    quantity_b: null,
+    weight: 120,
+    weight_b: null,
+    price: 105,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 11,
+    route: 'fantasia',
+    image_small: FantasiaS,
+    image_normal: FantasiaM,
+    image_large: FantasiaL,
+    title: 'Фантазия',
+    description:
+      'Легкий мусс со вкусом малины, манго и маракуйи',
+    ingredients:
+      'Сливки 33%, сахар, малина с/м, манго пюре, маракуйя пюре, яйцо куриное, мука пшеничная, гель нейтральный',
+    protein: 4.4,
+    fat: 24.5,
+    carbohydrates: 49.5,
+    calories: 433,
+    quantity: 16,
+    quantity_b: null,
+    weight: 100,
+    weight_b: null,
+    price: 100,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 12,
+    route: 'ptichie-moloko',
+    image_small: PtichieMolokoS,
+    image_normal: PtichieMolokoM,
+    image_large: PtichieMolokoL,
+    title: 'Птичье молоко',
+    description:
+      'Воздушное суфле на бисквитном корже под слоем горького шоколада',
+    ingredients:
+      'Масло сливочное, сахар, сгущенное молоко гост, шоколад 72%, мука пшеничная, яйцо куриное, масло растительное, агар-агар, лимонная кислота',
+    protein: 8.3,
+    fat: 23.8,
+    carbohydrates: 56.6,
+    calories: 473,
+    quantity: 16,
+    quantity_b: null,
+    weight: 70,
+    weight_b: null,
+    price: 83,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 13,
+    route: 'milfey-djandua',
+    image_small: MilfeyDjanduaS,
+    image_normal: MilfeyDjanduaM,
+    image_large: MilfeyDjanduaL,
+    title: 'Милфей джандуйя',
+    description:
+      'Медовые коржи с ореховым кремом джандуйя',
+    ingredients:
+      'Мука пшеничная, сахар, сливки 33%, масло сливочное 82%, сметана 20%, яйцо куриное, крем джандуйя, сгущенное молоко гост, мед, сода, уксус',
+    protein: 5.7,
+    fat: 26.1,
+    carbohydrates: 36.6,
+    calories: 404,
+    quantity: 15,
+    quantity_b: null,
+    weight: 70,
+    weight_b: null,
+    price: 88,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 14,
+    route: 'mindalnoe',
+    image_small: MindalnoeS,
+    image_normal: MindalnoeM,
+    image_large: MindalnoeL,
+    title: 'Миндальное',
+    description:
+      'Необычное сочетание миндаля с черносливом и грецким орехом совместно с нежным сливочным кремом придает незабываемый вкус',
+    ingredients:
+      'Сливки 33%, сахар, миндальная мука, грецкий орех, чернослив, масло сливочное 82%, яйцо куриное, крахмал, сахарная пудра',
+    protein: 5.5,
+    fat: 26.1,
+    carbohydrates: 36.4,
+    calories: 408,
+    quantity: 25,
+    quantity_b: null,
+    weight: 45,
+    weight_b: null,
+    price: 57,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 15,
+    route: 'kartoshka',
+    image_small: KartoshkaS,
+    image_normal: KartoshkaM,
+    image_large: KartoshkaL,
+    title: 'Картошка',
+    description:
+      'Известное пирожное приготовленное на бисквитной основе с коньяком, грецкими орехами, сливочным маслом и какао',
+    ingredients:
+      'Мука пшеничная, масло сливочное, сахар, яйцо куриное, джем клубничный, какао, орехи грецкие',
+    protein: 6.8,
+    fat: 20.4,
+    carbohydrates: 44.5,
+    calories: 377,
+    quantity: 30,
+    quantity_b: 25,
+    weight: 40,
+    weight_b: 80,
+    price: 28,
+    price_b: 52,
+    category: 'pastry',
+  },
+  {
+    id: 16,
+    route: 'karamelnoe',
+    image_small: KaramelnoeS,
+    image_normal: KaramelnoeM,
+    image_large: KaramelnoeL,
+    title: 'Карамельное',
+    description:
+      'Шоколадный бисквит и карамельный мусс придадут нотку настроения в чаепитие',
+    ingredients:
+      'Сливки33%, сахар, мука пшеничная ,яйцо куриное, какао алкализованный, шоколад молочный 34%, желатин, шарики вафельные',
+    protein: 2.7,
+    fat: 12,
+    carbohydrates: 52,
+    calories: 331,
+    quantity: 15,
+    quantity_b: null,
+    weight: 110,
+    weight_b: null,
+    price: 96,
+    price_b: null,
+    category: 'pastry',
+  },
+  {
+    id: 17,
+    route: 'bavarskoe',
+    image_small: BavarskoeS,
+    image_normal: BavarskoeM,
+    image_large: BavarskoeL,
+    title: 'Баварское',
+    description:
+      'Воздушный мусс из сливок и ванили, со свежей малиной во фруктовом желе',
+    ingredients:
+      'Сливки 33%, молоко, сахар, яйцо куриное, мука пшеничная, малина с/м, гель нейтральный, желатин',
+    protein: 2.9,
+    fat: 14.4,
+    carbohydrates: 39,
+    calories: 259,
+    quantity: 10,
+    quantity_b: 15,
+    weight: 100,
+    weight_b: 170,
+    price: 83,
+    price_b: 104,
+    category: 'pastry',
+  },
+  {
+    id: 18,
+    route: 'maraquya',
+    image_small: MaraquyaS,
+    image_normal: MaraquyaM,
+    image_large: MaraquyaL,
+    title: 'Маракуйя',
+    description:
+      'Сливочно-фисташковый бисквит с легким муссом из свежей маракуйи и манго, покрытый желе из маракуйи',
+    ingredients:
+      'Пюре маракйи, сливки 33%, желатин гранулированный, фисташки, гель нейтральный, маракуйя, пюре манго, яйцо куриное, миндаль, мука пшеничная, сахар',
+    protein: 2.7,
+    fat: 9.6,
+    carbohydrates: 40.7,
+    calories: 223,
+    quantity: 16,
+    quantity_b: 16,
+    weight: 70,
+    weight_b: 100,
+    price: 80,
+    price_b: 96,
+    category: 'pastry',
+  },
+  {
+    id: 19,
+    route: 'tri-shokolada',
+    image_small: TriShokoladaS,
+    image_normal: TriShokoladaM,
+    image_large: TriShokoladaL,
+    title: 'Три шоколада',
+    description:
+      'Элегантное трио белого, молочного и черного шоколада в сочетании нежнейшего шоколадного бисквита',
+    ingredients:
+      'Шоколад белый 34%, шоколад темный 53%, сахар, сливки молочные 33%, яйцо куриное, мука пшеничная, какао, желатин',
+    protein: 3.1,
+    fat: 19,
+    carbohydrates: 43.7,
+    calories: 302,
+    quantity: 16,
+    quantity_b: 16,
+    weight: 70,
+    weight_b: 105,
+    price: 82,
+    price_b: 103,
     category: 'pastry',
   },
 ];
